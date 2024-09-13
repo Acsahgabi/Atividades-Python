5cfdabdad26a8a010cc12c652f11b26e92f8b075
Acsahgabi <acsatrindade458@gmail.com>
2024-09-13 10:55:16 -0300

    Create Atividade1.py
    Subindo a primeira atividade

---
+++ EstruturaDeRepetição/Atividade1.py
@@ -0,0 +1,4 @@
+"""Faça um programa que calcule a média de salários de uma empresa, pedindo ao usuário a quantidade de funcionários,
+o nome e o salário de cada funcionário e devolvendo a média, 
+o salário mais alto e o salário mais baixo."""
+
